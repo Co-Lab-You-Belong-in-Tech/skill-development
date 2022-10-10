6d7b6e1f927bebfa7f600dd181a1401d09a109b4
Somdotta07 <jhilick.s@gmail.com>
2022-10-10 14:54:23 +0200

    Basic set up for the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,56 @@ import React, { FunctionComponent } from "react";
 const Home: FunctionComponent = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <p>
+        I am a{" "}
+        <span>
+          <input type="text" id="job" name="job" />
+        </span>
+      </p>
+      <div className="skills">
+        <h2>Choose your top 5 skills</h2>
+        <div>
+          <button>Report writing</button>
+          <button>Digital Marketing</button>
+          <button>Data Analysis</button>
+          <button>Audience Insights</button>
+          <button>Social Listening</button>
+          <button>Content Writing</button>
+          <button>Front-end Development</button>
+          <button>Full-stack Development</button>
+          <button>Public Speaking</button>
+          <button>Back-end Development</button>
+        </div>
+        <p>
+          <em>Finding tech roles for you..</em>
+          <button>Refresh Roles</button>
+        </p>
+      </div>
+      <div className="roles">
+        <div className="share_roles">
+          <p>Top tech roles based on skills associated with your role</p>
+          <p>Share your mathes</p>
+        </div>
+        <div className="list_roles">
+          <ul>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+            <li>UX Writer</li>
+          </ul>
+        </div>
+      </div>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+          </form>
+          <footer>The End</footer>
     </div>
   );
 };
